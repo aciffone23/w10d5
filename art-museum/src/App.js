9a64f75b633971bfd765070ec6edbc88d4cc28cf
Jiamin-Zou <jiaminz622@gmail.com>
2023-05-12 15:56:52 -0400

    Completed phase 1 Gallery Navigation

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -2,8 +2,6 @@ import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 
 function App() {
-  
-  
     return <GalleryNavigation galleries={harvardArt.records}/>
   
   // return [galleries]
--- art-museum/src/components/GalleryNavigation/index.js
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -1,7 +1,27 @@
-// import harvardArt from "../../data/harvardArt";
-const GalleryNavigation = props =>  {
-    console.log(props)
-    return <h1>Galleries</h1>
+import { NavLink } from 'react-router-dom';
+import "./GalleryNavigation.css";
+
+const GalleryNavigation = ({galleries}) =>  {
+    const GalleryItem = galleries.map((gallery) => {
+        return (
+            <li>
+                <NavLink to={`/galleries/${gallery.id}`} key={gallery.id} className="gallery-item">
+                    {gallery.name}
+                </NavLink>
+            </li>
+        )
+    })
+    return (
+        <nav>
+            <h1>Galleries</h1>
+            <NavLink to="/" exact>
+                Home Page
+            </NavLink>
+            <ul>
+                {GalleryItem}
+            </ul>
+        </nav>
+    )
 }
 
 export default GalleryNavigation;
